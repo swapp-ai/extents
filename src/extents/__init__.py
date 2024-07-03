067c8cd777097b48aca3012d84af50350c7f4198
Michael Frank <michaelfrank@swapp.net>
2024-07-03 08:35:29 +0300

    move to src layout

--- src/extents/__init__.py
+++ src/extents/__init__.py
@@ -1,1 +1,17 @@
+import importlib.metadata
+import pathlib
+
+
+__version__ = importlib.metadata.version("swp-extents")
+""" Package version -- read only value """
+
+
+__version_tuple__ = tuple(__version__.split('.'))
+""" Package version tuple -- read only value """
+
+
+__package_root__ = pathlib.Path(__file__).parent.parent
+""" Path of package root in filesystem -- read only value """
+
+
 from .extents import *
